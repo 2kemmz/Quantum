b6dbb3be8bf071bfef8b14b81a26cae931857cc4
2kemmz <129622095+2kemmz@users.noreply.github.com>
2025-04-09 19:56:05 +0100

    Create main.lua

---
+++ manager/main.lua
@@ -0,0 +1,30 @@
+if game.PlaceId == (891852901) then
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Quantum";
+Text = "Greenville";
+Duration = 4;
+})
+wait(5)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/LUAScripts/refs/heads/main/paidscripts/Greenville.lua'),true))()
+
+elseif game.PlaceId == (18722757961) then
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Quantum";
+Text = "Ragblood";
+Duration = 4;
+})
+wait(5)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/LUAScripts/refs/heads/main/freescripts/Ragblood.lua'),true))()
+
+elseif game.PlaceId == (4637668954) then
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Quantum";
+Text = "Rensselaer County";
+Duration = 4;
+})
+wait(5)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/LUAScripts/refs/heads/main/paidscripts/RensselaerCounty.lua'),true))()
+end
