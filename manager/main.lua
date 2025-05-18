f39fc86a27d79401cb0fa4e764a09b46faa94c11
2kemmz <129622095+clipsbyj@users.noreply.github.com>
2025-05-18 16:25:33 +0100

    Update main.lua

--- manager/main.lua
+++ manager/main.lua
@@ -6,7 +6,7 @@ Text = "Quantum successfully loaded.";
 Duration = 4;
 })
 wait(0.1)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/Quantum/refs/heads/main/freescripts/Ragblood.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/clipsbyj/Quantum/refs/heads/main/freescripts/Ragblood.lua'),true))()
 
 elseif game.PlaceId == (891852901) then
 wait(0.1)
@@ -16,7 +16,7 @@ Text = "Quantum successfully loaded.";
 Duration = 4;
 })
 wait(0.1)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/Quantum/refs/heads/main/paidscripts/Greenville.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/clipsbyj/Quantum/refs/heads/main/paidscripts/Greenville.lua'),true))()
 
 elseif game.PlaceId == (8416011646) then
 wait(0.1)
@@ -26,7 +26,7 @@ Text = "Quantum successfully loaded.";
 Duration = 4;
 })
 wait(0.1)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/Quantum/refs/heads/main/paidscripts/MapleCounty.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/clipsbyj/Quantum/refs/heads/main/paidscripts/MapleCounty.lua'),true))()
 
 elseif game.PlaceId == (2534724415) then
 wait(0.1)
@@ -36,7 +36,7 @@ Text = "Quantum successfully loaded.";
 Duration = 4;
 })
 wait(0.1)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/Quantum/refs/heads/main/paidscripts/EmergencyResponse.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/clipsbyj/Quantum/refs/heads/main/paidscripts/EmergencyResponse.lua'),true))()
 
 elseif game.PlaceId == (4637668954) then
 wait(0.1)
@@ -46,7 +46,7 @@ Text = "Quantum successfully loaded.";
 Duration = 4;
 })
 wait(0.1)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/Quantum/refs/heads/main/paidscripts/RensselaerCounty.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/clipsbyj/Quantum/refs/heads/main/paidscripts/RensselaerCounty.lua'),true))()
 
 elseif game.PlaceId == (87720117989607) then
 wait(0.1)
@@ -56,7 +56,7 @@ Text = "Quantum successfully loaded.";
 Duration = 4;
 })
 wait(0.1)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/Quantum/refs/heads/main/paidscripts/Ridgewood.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/clipsbyj/Quantum/refs/heads/main/paidscripts/Ridgewood.lua'),true))()
 
 elseif game.PlaceId == (5104202731) then
 wait(0.1)
@@ -66,5 +66,5 @@ Text = "Quantum successfully loaded.";
 Duration = 4;
 })
 wait(0.1)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/2kemmz/Quantum/refs/heads/main/paidscripts/SouthwestFlorida.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/clipsbyj/Quantum/refs/heads/main/paidscripts/SouthwestFlorida.lua'),true))()
 end
